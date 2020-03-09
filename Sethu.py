01eef1bf3ce0d0589dbdbe0daad0fcdad0e071ed
Derek Gwekwerere <makoregwekwerere@gmail.com>
2020-03-09 16:43:05 +0200

    another one

---
+++ Sethu.py
@@ -0,0 +1,8 @@
+import math
+#input values below
+a=8
+b=2
+c=1
+d=4
+#calculatio process
+print(math.sqrt(a-3)/(b**2+c**2+d**2))
---
+++ task 1.py
@@ -0,0 +1,5 @@
+print("Hello World")
+
+import math
+r=int(input("Enter Radius of the Circle:"))
+print("Area of the circle is",str(math.pi)*(r**2))
